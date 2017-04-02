3f7a9166d6ef05968eb7eff5f3780fce05e10581
Jakob Gerstenlauer <Jakob Gerstenlauer>
2017-04-02 13:51:14 +0200

    bugfix: use scores (loadings of individuals) instead of loadings of features for cluster analysis

--- explore_data.R
+++ explore_data.R
@@ -116,12 +116,6 @@ summary(m1.pls2)
 # Plot of R2 for each digit 
 plot(R2(m1.pls2), legendpos = "bottomright")
 
-#Inspect the object:
-dim(R2(m1.pls2)$val[1,,])
-# 10 257
-#rows: response
-#columns: cumulative number of components
-
 #calculate the mean R2 over all digits for an increasing number of components:
 r2.mean<-apply(R2(m1.pls2)$val[1,,],2,mean)
 
@@ -132,7 +126,7 @@ plot(1:50,r2.mean[1:50],pch="+")
 # based on generalized cross-validation:
 n <- nrow(Xs)
 #p <- ncol(Xs)
-p<-100
+p<-40
 q <- ncol(Y)
 
 R2cv<-rep(-1,p)
@@ -151,18 +145,21 @@ plot(1:p,R2cv,type="l",
      points(1:p,r2.mean[1:p],type="l",col="red")
 dev.off()
 
-#Conclusion: Based on generalized CV 40 components are sufficient.
-nc<-40
+#Conclusion: Based on generalized CV 25 components are sufficient.
+nc<-25
 
 ###################################################################################################################
 # Cluster analysis
 ###################################################################################################################
 
-#TODO Define the significant components here
+#the loadings of the inputs on the components
 Psi<-m1.pls2$loadings[,1:nc]
 
+#loadings of the individuals
+scores<-m1.pls2$scores[,1:nc]
+
 #... is transformed into a distance matrix.
-dist.matrix<-dist(Psi, method = "euclidean")
+dist.matrix<-dist(scores, method = "euclidean")
 
 #Then I perform hierarchical clustering based on this distance matrix
 #and the corrected Ward algorithm:
@@ -174,48 +171,154 @@ plot(clusters)
 dev.off()
 
 jpeg("hierarchical_clustering_WARD_inertia_explained.jpg")
-barplot(clusters$height[500:560])
-dev.off()
-#There is no obvious way to split the data into clusters!
-
-
-#Let's try with components that explain 80%.
-nc<-20
-#TODO Define the significant components here
-Psi<-m1.pls2$loadings[,1:nc]
-
-#... is transformed into a distance matrix.
-dist.matrix<-dist(Psi, method = "euclidean")
-
-#Then I perform hierarchical clustering based on this distance matrix
-#and the corrected Ward algorithm:
-clusters <- hclust(dist.matrix, method = "ward.D2")
-
-setwd(plotDir)
-jpeg("hierarchical_clustering_WARD_20_components.jpg")
-plot(clusters)
-dev.off()
-
-jpeg("hierarchical_clustering_WARD_inertia_explained_20_components.jpg")
 barplot(clusters$height)
 dev.off()
 
-barplot(clusters$height[550:560])
-
-cl <- cutree(clusters, 6)
+#It is pretty obvious that 3 splits / 4 clusters make sense!
+cl <- cutree(clusters, 4)
 
 setwd(plotDir)
-jpeg("hierarchical_clustering_ward_6classes_PC1_PC2.jpeg")
-plot(Psi[,1],Psi[,2],type="n",main="Clustering of observations into 6 classes")
+jpeg("hierarchical_clustering_ward_4classes_PC1_PC2.jpeg")
+plot(Psi[,1],Psi[,2],type="n",main="Clustering of observations into 4 classes")
 points(Psi[,1],Psi[,2],col=cl,pch=dy$V1, cex = 0.6)
 abline(h=0,v=0,col="gray")
-legend("topleft",c("c1","c2","c3","c4","c5","c6"),pch=20,col=c(1:4))
+legend("topleft",c("c1","c2","c3","c4"),pch=20,col=c(1:4))
 dev.off()
 
 setwd(plotDir)
-jpeg("hierarchical_clustering_ward_6classes_PC1_PC3.jpeg")
-plot(Psi[,1],Psi[,3],type="n",main="Clustering of observations into 6 classes")
+jpeg("hierarchical_clustering_ward_4classes_PC1_PC3.jpeg")
+plot(Psi[,1],Psi[,3],type="n",main="Clustering of observations into 4 classes")
 points(Psi[,1],Psi[,3],col=cl,pch=dy$V1, cex = 0.6)
 abline(h=0,v=0,col="gray")
-legend("topleft",c("c1","c2","c3","c4","c5","c6"),pch=20,col=c(1:4))
+legend("topleft",c("c1","c2","c3","c4"),pch=20,col=c(1:4))
 dev.off()
+
+#Is there any correspondence between the clusters and the labels?
+ct<-data.frame(labels=dy$V1,clusters=cl)
+table(ct)
+# clusters
+# labels    1    2    3    4
+#      1    0    0  953  273
+#      2    0    0  933  140
+#      3    0    0  287  699
+#      4 1264   21    1    0
+#      5 1373    1    0    0
+#      6   37 1369    1    0
+
+# cluster 1 -> label 3+4
+# cluster 2 -> label 6
+# cluster 3 -> label 1+2+ (3)
+# cluster 4 -> label (1+2)+3
+
+#Conclusion: 
+#The clusters map pretty well to the labels!
+
+#Now let's run a consolidated cluster analysis using the centroids of this partitioning:
+#Consolidation of the partition:
+
+#I use the centroids of the 4 clusters found with hierarchical clustering (WARD)
+#as starting point for k-means:
+
+#Calculate the centroids of the 4 clusters
+#in the nc significant dimensions (principal components)
+cdg <- aggregate(as.data.frame(scores),list(cl),mean)[,2:(nc+1)]
+
+k4 <- kmeans(scores, centers=cdg)
+
+Bss <- sum(rowSums(k4$centers^2)*k4$size) # = k5$betweenss
+Wss <- sum(k4$withinss) # = k5$tot.withinss
+(Ib5 <- 100*Bss/(Bss+Wss))
+#[1] 72.88688
+
+#Is there any correspondence between the clusters and the labels?
+ct2<-data.frame(labels=dy$V1,clusters=k4$cluster)
+table(ct2)
+# clusters
+# labels    1    2    3    4
+# 1    0    0  905  321
+# 2    0    0  971  102
+# 3    0    0  356  630
+# 4 1192   91    3    0
+# 5 1373    0    1    0
+# 6   21 1375   11    0
+
+
+#********************************************
+#Now let's use five clusters (5 labels!)
+#********************************************
+cl <- cutree(clusters, 5)
+
+setwd(plotDir)
+jpeg("hierarchical_clustering_ward_5classes_PC1_PC2.jpeg")
+plot(Psi[,1],Psi[,2],type="n",main="Clustering of observations into 4 classes")
+points(Psi[,1],Psi[,2],col=cl,pch=dy$V1, cex = 0.6)
+abline(h=0,v=0,col="gray")
+legend("topleft",c("c1","c2","c3","c4","c5"),pch=20,col=c(1:4))
+dev.off()
+
+setwd(plotDir)
+jpeg("hierarchical_clustering_ward_5classes_PC1_PC3.jpeg")
+plot(Psi[,1],Psi[,3],type="n",main="Clustering of observations into 4 classes")
+points(Psi[,1],Psi[,3],col=cl,pch=dy$V1, cex = 0.6)
+abline(h=0,v=0,col="gray")
+legend("topleft",c("c1","c2","c3","c4","c5"),pch=20,col=c(1:4))
+dev.off()
+
+#Is there any correspondence between the clusters and the labels?
+ct<-data.frame(labels=dy$V1,clusters=cl)
+table(ct)
+# clusters
+# labels    1    2    3    4    5
+# 1    0    0  953  250   23
+# 2    0    0  933  140    0
+# 3    0    0  287  574  125
+# 4 1264   21    1    0    0
+# 5 1373    1    0    0    0
+# 6   37 1369    1    0    0
+
+# cluster 1 -> label 4+5
+# cluster 2 -> label 6
+# cluster 3 -> label 1+2+ (3)
+# cluster 4 -> label (1+2)+3
+# cluster 5 -> label 3
+
+#Now let's run a consolidated cluster analysis using the centroids of this partitioning:
+#Consolidation of the partition:
+
+#I use the centroids of the 5 clusters found with hierarchical clustering (WARD)
+#as starting point for k-means:
+
+#Calculate the centroids of the 5 clusters
+#in the nc significant dimensions (principal components)
+cdg <- aggregate(as.data.frame(scores),list(cl),mean)[,2:(nc+1)]
+
+k5 <- kmeans(scores, centers=cdg)
+
+Bss <- sum(rowSums(k5$centers^2)*k5$size) # = k5$betweenss
+Wss <- sum(k5$withinss) # = k5$tot.withinss
+(Ib5 <- 100*Bss/(Bss+Wss))
+#[1] 75.45319
+
+ct2<-data.frame(labels=dy$V1,clusters=k5$cluster)
+table(ct2)
+# clusters
+# labels    1    2    3    4    5
+# 1    0    0  692  483   51
+# 2    0    0  861  208    4
+# 3    0    0  185  629  172
+# 4 1192   89    5    0    0
+# 5 1373    0    1    0    0
+# 6   21 1373   13    0    0
+
+# cluster 1 -> label 4+5
+# cluster 2 -> label 6
+# cluster 3 -> label 1+2+ (3)
+# cluster 4 -> label 1+(2)+3
+# cluster 5 -> label 3
+
+#********************************************
+#Next step: 
+# Use relevance vector machine or support vector machine
+# to predict the label 
+# using the 25 significant components as input!
+#********************************************
