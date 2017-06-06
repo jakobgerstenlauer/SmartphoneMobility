99505489c364cd2e126baea9dd212ce6e9198aaa
jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>
2017-06-06 23:22:55 +0200

    remove file with paths

--- workingDir.R
+++
@@ -1,6 +0,0 @@
-workingDir<-"~/Documents/UPC/2017/01/MV/project"
-codeDir<-glue(workingDir,"/SmartphoneMobility")
-dataDir<-glue(workingDir,"/data")
-testDir<-glue(dataDir,"/test")
-trainDir<-glue(dataDir,"/train")
-plotDir<-glue(workingDir,"/plots")
